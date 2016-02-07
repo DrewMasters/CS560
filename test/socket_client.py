f55ed2e1bdda397592a36672913fd2a5f9d3a3fc
Drew Masters <drewmasters78@gmail.com>
2016-02-07 13:03:29 -0500

    socket programs handle sending and receiving of messages

--- test/socket_client.py
+++ test/socket_client.py
@@ -1,20 +1,29 @@
 #simple client program
 #www.tutorialspoint.com/python/python_networking.htm
 
+#usage: python socket_client.py hostname (other host machine)
+#	or
+#	python socket_client.py (local machine)
+
 import sys
 import socket
 
+def send_receive(sock):
+	command=raw_input('terminal: ')
+	while command.lower() != 'exit':
+		sock.send(command)
+		print sock.recv(1024)
+		command=raw_input('terminal: ')
+	sock.send('done')
+	return
+	
+s = socket.socket()
 if len(sys.argv) == 1:
-	s = socket.socket()
 	host = socket.gethostname()
-	port = 12345
-	s.connect((host,port))
-	print s.recv(1024)
-	s.close()
 else:
-	s = socket.socket()
 	host = str(sys.argv[1])
-	port = 12345
-	s.connect((host,port))
-	print s.recv(1024)
-	s.close()
+port = 12345
+s.connect((host,port))
+print s.recv(1024)
+send_receive(s)
+s.close()
--- test/socket_server.py
+++ test/socket_server.py
@@ -1,7 +1,18 @@
 #simple test server in python
 #www.tutorialspoint.com/python/python_networking.htm
 
+#usage: python socket_server.py &
+
 import socket
+
+def receive_send(sock):
+	t=sock.recv(1024)	
+	print t
+	sock.send(t)
+	if t=='done':
+		return False
+	else:
+		return True
 
 s=socket.socket()
 host = socket.gethostname()
@@ -13,3 +24,6 @@ while True:
 	c, addr = s.accept()
 	print 'Got connection from', addr
 	c.send('connected')
+	flag = receive_send(c)
+	while flag:
+		flag = receive_send(c)
