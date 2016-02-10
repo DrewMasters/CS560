9e3f79b90938e7fc36bb167f4e32b37a4fe2ec10
Joseph Dorris <jdorris1331@gmail.com>
2016-02-10 15:16:58 -0500

    started free list set

--- file_system.h
+++ file_system.h
@@ -5,9 +5,15 @@ struct inode {
   char file_name[100];
 };
   
-
 struct file_system {
   struct inode inode_list[1000];
-  //free_list
-  long memory;
+  char free_list[3125];
 };
+
+void set_page_free(struct file_system *, long num) {
+//or with 1
+}
+
+void set_page_used(struct file_system *, long num) {
+// and with ~1
+}
