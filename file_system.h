d5e1a25cad3d1b2b9c89861ab50945c29ce18dee
Drew Masters <drewmasters78@gmail.com>
2016-02-10 20:19:56 +0000

    wrote set and free functions for free list

--- file_system.h
+++ file_system.h
@@ -12,8 +12,14 @@ struct file_system {
 
 void set_page_free(struct file_system *, long num) {
 //or with 1
+  div_eight=num/8;
+  mod_eight=num%8;
+  file_system->inode_list[div_eight]&=~(1<<(7-mod_eight));
 }
 
 void set_page_used(struct file_system *, long num) {
 // and with ~1
+  div_eight=num/8;
+  mod_eight=num%8;
+  file_system->inode_list[div_eight]|=(1<<(7-mod_eight));
 }
