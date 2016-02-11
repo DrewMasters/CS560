303ddb8426bcddf37acde27d88698f5c42f40311
Drew Masters <drewmasters78@gmail.com>
2016-02-11 13:54:12 +0000

    added more comments

--- file_system.h
+++ file_system.h
@@ -18,7 +18,7 @@ void set_page_free(struct file_system *fs, long num) {
 //or with 1
   int div_eight=num/8;
   int mod_eight=num%8;
-  f_s->free_list[div_eight]&=~(1<<(7-mod_eight));
+  fs->free_list[div_eight]&=~(1<<(7-mod_eight));
 }
 
 int find_first_free_page(struct file_system *fs)
@@ -43,6 +43,7 @@ int find_first_free_page(struct file_system *fs)
 
   set_page_used(fs, pos*8+j); //set the page as taken
   //should we just move the set_page_used function in here?
+  //fs->free_list[pos]|=(1<<(7-j)); //set page as being used
 
   return pos*8+j; //first free page number 
 }
@@ -51,5 +52,5 @@ void set_page_used(struct file_system *fs, long num) {
 // and with ~1
   int div_eight=num/8;
   int mod_eight=num%8;
-  f_s->free_list[div_eight]|=(1<<(7-mod_eight));
+  fs->free_list[div_eight]|=(1<<(7-mod_eight));
 }
