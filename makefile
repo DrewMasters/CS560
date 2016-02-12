b41cbd803dba09484029462570f3572287d0593a
Joseph Dorris <jdorris1331@gmail.com>
2016-02-12 14:44:49 -0500

    working on makefile

--- makefile
+++ makefile
@@ -1,4 +1,17 @@
 all: file_system
 
-file_system:
-	gcc -o file_system -I../include ./src/file_system.c
+OBJS=file_system.o mkfs.o
+LIB=libfs.a
+INC=include
+
+file_system.o:
+	gcc -c -I$(INC) src/file_system.c
+
+mkfs.o:
+	gcc -c -I$(INC) src/mkfs.c
+
+$(LIB): $(OBJS)
+	ar rcs libfs.a $(OBJS)
+
+file_system: $(LIB)
+	gcc -o file_system -I./include -L./lib/file_system.a ./src/file_system.c
---
+++ src/file_system.c
@@ -0,0 +1,42 @@
+#include "file_system.h"
+
+void set_page_free(struct file_system *fs, long num) {
+//or with 1
+  int div_eight=num/8;
+  int mod_eight=num%8;
+  fs->free_list[div_eight]&=~(1<<(7-mod_eight));
+}
+
+int find_first_free_page(struct file_system *fs)
+{
+//finds first free page in free list
+  int pos = 0; //position in array with an open array
+  int i;
+  int j=0; //position of first zero in number
+
+  for (i=0; i < NUM_FREE_LIST_BYTES; i++)
+  {
+    if (fs->free_list[i]!=255)
+    {
+      pos = i;
+      break;
+    }
+  }
+
+  while ((fs->free_list[pos]>>j)%2==1){
+    j++;
+  }
+
+  set_page_used(fs, pos*8+j); //set the page as taken
+  //should we just move the set_page_used function in here?
+  //fs->free_list[pos]|=(1<<(7-j)); //set page as being used
+
+  return pos*8+j; //first free page number 
+}
+
+void set_page_used(struct file_system *fs, long num) {
+// and with ~1
+  int div_eight=num/8;
+  int mod_eight=num%8;
+  fs->free_list[div_eight]|=(1<<(7-mod_eight));
+}
